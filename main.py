11dbf41109fdc2cb3daa9dc889a6a18e39c666f9
costieraleltsuso <mocka2@hotmail.com>
2026-04-03 22:07:40 +0100

    B: added prompt token and a response token usage statment

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 def main():
-    print("Hello from aislupain!")
     load_dotenv()
+    print("Hello from aislupain!")
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
     if api_key is None:
-        raise RuntimeError("the api key is not sey yet")
+        raise RuntimeError("the api key is not set yet")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model='gemini-2.5-flash',contents=f"{args.user_prompt}")
+
+    meta_data = response.usage_metadata
+    prompt_count = meta_data.prompt_token_count
+    can_token_count = meta_data.candidates_token_count
+
+    if prompt_count is None and can_token_count is None:
+        raise RuntimeError("API call failed")
+    print(f"")
+    print(f"Prompt tokens: {prompt_count}")
+    print(f"Response tokens: {can_token_count}")
+    print(f"Response:")
+    print(response.text)
 
 
 

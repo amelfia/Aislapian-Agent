2ac5d7b206043bda6d101aab5b70460551b3f7c7
costieraleltsuso <mocka2@hotmail.com>
2026-04-27 13:08:25 +0100

    H: Agent can read the content of files with limited access to directories'

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,25 +1,29 @@
 import os
-from config import  MAX_CHARS
 
 def get_file_content(working_directory, file_path):
+    MAX_CHARS = 10000
     try:
         working_directory_abs = os.path.abspath(working_directory)
-        target_file_path = os.path.normpath(os.path.join(working_directory, file_path))
+        target_file_path = os.path.normpath(os.path.join(working_directory_abs, file_path))
 
-        if os.path.commonpath([working_directory_abs, target_file_path]) != working_directory
+        if os.path.commonpath([working_directory_abs, target_file_path]) != working_directory_abs:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working'
 
         if not os.path.isfile(target_file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
 
 
-        f = file_path.read(MAX_CHARS)
-        f.read()
-        if f.read(1):
-            content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        with open(target_file_path,"r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return file_content_string
 
     except Exception as E:
         return f"Error reading files: {E}"
+
 
 
 
